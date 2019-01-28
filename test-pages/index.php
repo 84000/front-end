a558bf4b4106b95aa3a197f92eaf916890334dca
domlatham <dominiclatham@gmail.com>
2019-01-28 11:34:37 +0100

    Both header and footer generated from XML

--- test-pages/index.php
+++ test-pages/index.php
@@ -2,6 +2,29 @@
 
     $content_template = array_key_exists('template', $_GET) ? $_GET['template'] : 'home';
 
+    // Get the stylesheet
+    $xsl = new DOMDocument;
+    $xsl->load('../xslt/84000-html.xsl');
+
+    // Initiate the processor
+    $processor = new XSLTProcessor;
+    $processor->importStyleSheet($xsl);
+
+    $processor->setParameter('', 'lang', isset($_GET['lang']) && $_GET['lang'] == 'zh' ? 'zh' : 'en' );
+    $active_url = 'http://84000.co' . substr($_SERVER['REQUEST_URI'], strlen('/test-pages'));
+    $processor->setParameter('', 'active-url', $active_url);
+    $processor->setParameter('', 'local-comms-url', '/test-pages');
+    $processor->setParameter('', 'local-reading-room-url', 'http://read.84000-translate.org');
+    $processor->setParameter('', 'local-front-end-url', '');
+    $processor->setParameter('', 'default-search-form-action', 'comms');
+
+    // Get the xml
+    $headerXML = new DOMDocument;
+    $headerXML->load('../xslt/84000-header.xml');
+
+    $footerXML = new DOMDocument;
+    $footerXML->load('../xslt/84000-footer.xml');
+
 ?>
 
 <!DOCTYPE html>
@@ -32,125 +55,16 @@
     </head>
 
     <body id="top">
-        <nav class="navbar navbar-default">
 
-            <div class="brand-header">
-                <div class="container">
-                    <div class="navbar-header">
-                        <div class="navbar-brand center-vertical">
-
-                            <a href="/test-pages/" class="logo">
-                                <img src="/imgs/logo.png">
-                            </a>
-
-                            <span class="tag-line">
-                                Translating the words of the Buddha
-                            </span>
-
-                            <span class="nav-button">
-                                <button id="navigation-button" class="btn-round navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-                                    <i class="fa fa-bars" aria-hidden="true"></i>
-                                </button>
-                            </span>
-
-                        </div>
-                    </div>
-                </div>
-                
-            </div>
-
-            <div class="container">
-                <div id="navbar" class="navbar-collapse collapse" aria-expanded="false">
-                    
-                    <?php
-
-                    // Get the stylesheet
-                    $xsl = new DOMDocument;
-                    $xsl->load('../navigation/navigation.xsl');
-
-                    // Initiate the processor
-                    $processor = new XSLTProcessor;
-                    $processor->importStyleSheet($xsl);
-
-                    // Get the xml
-                    $xml = new DOMDocument;
-                    $xml->load('../navigation/navigation.xml');
-
-                    //$processor->setParameter('', 'lang', 'zh');
-                    $active_url = 'http://84000.co' . substr($_SERVER['REQUEST_URI'], strlen('/test-pages'));
-                    $processor->setParameter('', 'active-url', $active_url);
-                    $processor->setParameter('', 'local-comms-url', '/test-pages');
-                    $processor->setParameter('', 'local-reading-room-url', 'http://read.84000-translate.org');
-
-                    echo $processor->transformToXML($xml);
-
-                    ?>
-
-                    <form class="navbar-form navbar-right">
+        <?php
                         
-                        <div id="search-controls" class="input-group">
-                            <input type="text" name="s" class="form-control" placeholder="Search..."/>
-                            <span class="input-group-btn">
-                                <button class="btn btn-primary" type="button">
-                                    <i class="fa fa-search"></i>
-                                </button>
-                            </span>
-                        </div>
-
-                        <div id="language-links">
-                            <a href="/test-pages/">English</a> | <a href="/test-pages/ch">中文</a>
-                        </div>
-
-                    </form>
-
-                    <div class="navbar-right">
-                        <div id="social" class="center-vertical">
-                            <span>Follow our work</span>
-                            <a href="mailto:info@84000.co" target="_blank"><i class="fa fa-envelope-square" aria-hidden="true"></i></a>
-                            <a href="http://www.facebook.com/Translate84000" target="_blank"><i class="fa fa-facebook-square" aria-hidden="true"></i></a>
-                            <a href="https://twitter.com/Translate84000" target="_blank"><i class="fa fa-twitter-square" aria-hidden="true"></i></a>
-                            <a href="http://www.youtube.com/Translate84000" target="_blank"><i class="fa fa-youtube-square" aria-hidden="true"></i></a>
-                            <span>Chat</span>
-                            <a href="http://mp.weixin.qq.com/s/Y0K4_5CzA9oLtdflsJZfSQ" target="_blank"><i class="fa fa-weixin" aria-hidden="true"></i></a>
-                        </div>
-                    </div>
-                    
-                </div>
-            </div>
-
-        </nav>
-
-        <?php 
+            echo $processor->transformToXML($headerXML);
 
             include($content_template . ".php");
+                        
+            echo $processor->transformToXML($footerXML);
 
         ?>
-
-        <div class="hidden-print">
-            <div id="link-to-top-container" class="fixed-btn-container">
-                <a href="#top" id="link-to-top" class="btn-round scroll-to-anchor" title="Return to the top of the page">
-                    <i class="fa fa-arrow-up" aria-hidden="true"></i>
-                </a>
-            </div>
-        </div>
-
-        <footer xmlns="http://www.w3.org/1999/xhtml" class="hidden-print">
-            <div class="container" itemscope="itemscope" itemtype="http://schema.org/Organization">
-                Copyright © 2011-2018 <span itemprop="name">84000: Translating the Words of the Buddha</span> - All Rights Reserved
-                <br/>Contact: <a href="mailto:info@84000.co" itemprop="email">info@84000.co</a> | Website: <a href="http://84000.co" itemprop="url">http://84000.co</a>
-            </div>
-        </footer>
-
-        <span id="media_test">
-            <span class="visible-xs"></span>
-            <span class="visible-sm"></span>
-            <span class="visible-md"></span>
-            <span class="visible-lg"></span>
-            <span class="visible-print"></span>
-            <span class="visible-mobile"></span>
-            <span class="visible-desktop"></span>
-            <span class="event-hover"></span>
-        </span>
 
         <script type="text/javascript">
             function downloadJSAtOnload() {

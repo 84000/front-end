d0b37fc7fab91ab86cf805b704eb5525d9e569cd
domlatham <dominiclatham@gmail.com>
2019-04-01 12:18:17 +0200

    panel-img-header fix

--- js/84000.js
+++ js/84000.js
@@ -1893,7 +1893,7 @@ jQuery(document).ready(function($) {
 	// On loading the page...
 	// Position blockquote in header
 	// -----------------------------------------
-	$(".panel.main-panel > .panel-img-header+.panel-body blockquote:first-child").each(function(){
+	$(".panel.main-panel > .panel-img-header.has-img+.panel-body blockquote:first-child").each(function(){
 		if(!$("html.xs").length){
 			var $this = $(this);
 			$this.css({'top': -($this.outerHeight()) });

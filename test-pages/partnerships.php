f0906395a4fb973de8a054745f6ab9cabb85eca7
domlatham <dominiclatham@gmail.com>
2019-04-29 13:28:53 +0200

    prototype partnerships page

---
+++ test-pages/partnerships.php
@@ -0,0 +1,188 @@
+<div class="container">
+
+    <div class="row">
+        <div class="col-md-9 col-md-merge-right">
+            <div class="panel panel-about main-panel foreground">
+                <div class="panel-img-header">
+                    <h1>Partnerships</h1>
+                </div>
+                <div class="panel-body">
+                    <p>
+                        84000 works with partner organizations on specific projects or in collaborative ways that otherwise enrich our efforts, provide opportunities for scholarly coordination, and support the realization of our vision.
+                    </p>
+                    <hr/>
+                    <h2>University Partnerships</h2>
+                    <div class="row">
+                        <div class="col-sm-12">
+                            <h3>University of Virginia</h3>
+                        </div>
+                        <div class="col-sm-9">
+                            <p>
+                                Central to this collaboration is the placement of 84000’s editor-in-chief, James Gentry, within the university’s Department of Religious Studies. James’ role within the university is to lead the Tibetan Buddhist translation project that is 84000’s core mission, as well as to work on encyclopedic studies of Buddhist meditative systems found in those canons as a collaboration between 84000 and the university’s Center of Contemplative Studies. 
+                            </p>
+                            <p>
+                                This partnership allows for the provision of technical support, university facilities, software, Tibetan text editions, reference resources, and a platform in support of the work of translating the Tibetan Buddhist Canon. 
+                            </p>
+                            <p>
+                                For more information on the UVA’s Department of Religious Studies, please visit: <a href="#">www.?</a>.
+                            </p>
+                        </div>
+                        <div class="col-sm-3">
+                            <div class="thumbnail">
+                                <img src="/imgs/partner-logos/University_of_Virginia_1819_logo-700x700.png" class="img-responsive">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-sm-12">
+                            <h3>Rangjung Yeshe Institute</h3>
+                        </div>
+                        <div class="col-sm-9">
+                            <p>
+                                Founded by Chökyi Nyima Rinpoche and a group of his close students in 1997 the Rangjung Yeshe Institute is a center of higher learning in Nepal, working to the highest standards to provide both traditional and modern Buddhist education through teaching, translation, publication, research, and practice.
+                            </p>
+                            <p>
+                                Through this partnership, a joint 84000-RYI office has been established in Kathmandu to promote interaction and collaboration between 84000 translators and RYI translators, faculty, and students; and through which we may offer joint lectures, workshops, and seminars on topics related to Buddhist translation.
+                            </p>
+                            <p>
+                                For more information on Rangjung Yeshe Institute, please visit: <a href="#">www.?</a>.
+                            </p>
+                        </div>
+                        <div class="col-sm-3">
+                            <div class="thumbnail">
+                                <img src="/imgs/partner-logos/Rangjung_Yeshe_Institute_Logo.jpg" class="img-responsive">
+                            </div>
+                        </div>
+                    </div>
+                    <hr/>
+                    <h2>Collaborators</h2>
+                    <div class="row">
+                        <div class="col-sm-12">
+                            <h3>Khyentse Foundation</h3>
+                        </div>
+                        <div class="col-sm-9">
+                            <p>
+                                Khyentse Foundation is a nonprofit organization founded in 2001 by Dzongsar Khyentse Rinpoche to support all traditions of Buddhist study and practice. Its activities include major text preservation and translation projects, support for traditional monastic colleges in Asia, a worldwide scholarship and awards program, academic development of Buddhist studies in major universities, training and development for Buddhist leaders and teachers, Buddhist education for children, support for individual study and retreats, and more. 
+                            </p>
+                            <p>
+                                84000 began as an initiative of the Khyentse Foundation, and was incubated by the Foundation for xx years, before launching as its own independent non-profit in 2013. 84000 and Khyentse Foundation continue to enjoy a close working relationship xxxxxxx. 
+                            </p>
+                            <p>
+                                In 2019, Khyentse Foundation launched a new project YMFZ that ….. 84000 has been instrumental in sharing planning documents, lessons learned, procedural protocol, and other experience and information with the new project. 
+                            </p>
+                            <p>
+                                For more information on Khyentse Foundation, please visit: <a href="http://www.khyentsefoundation.org">www.khyentsefoundation.org</a>
+                            </p>
+                        </div>
+                        <div class="col-sm-3">
+                            <div class="thumbnail">
+                                <img src="/imgs/partner-logos/KhyentseLogo.png" class="img-responsive">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-sm-12">
+                            <h3>Adarsha</h3>
+                        </div>
+                        <div class="col-sm-9">
+                            <p>
+                                ADARSHA is an app named by H.H. 17th Karmapa Ogyen Trinley Dorje, that lets users read and conduct searches—in Unicode Tibetan or Wylie—of ancient Buddhist texts in a digital format, namely: the Kangyur (the words of the Buddha translated into Tibetan); the Tengyur (commentaries by Indian scholars translated into Tibetan); and Tibetan Buddhist scriptures. 
+                            </p>
+                            <p>
+                                Through this collaboration, 84000 has provided Adarsha with English translations of the titles of all texts in the Kangyur. This is a great step towards providing continuity across English-language referencing of the words of the Buddha.
+                            </p>
+                            <p>
+                                For more information on Adarsha, please visit: <a href="https://adarsha.dharma-treasure.org/about">https://adarsha.dharma-treasure.org/about</a> 
+                            </p>
+                        </div>
+                        <div class="col-sm-3">
+                            <div class="thumbnail">
+                                <img src="/imgs/partner-logos/adarsha.png" class="img-responsive">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-sm-12">
+                            <h3>Siddhartha’s Intent</h3>
+                        </div>
+                        <div class="col-sm-9">
+                            <p>
+                                Founded in 1986 by Dzongsar Jamyang Khyentse Rinpoche, Siddhartha’s Intent (SI) supports Rinpoche’s buddhadharma activities worldwide by organizing teachings and retreats, distributing and archiving recorded teachings, transcribing, editing and translating manuscripts and practice texts, and establishing a global community committed to continual study and practice. 
+                            </p>
+                            <p>
+                                Siddhartha’s Intent has been an integral support to 84000 in much of its global event planning, such as is necessary for our 84000 Sūtra Resoundings, and in helping us to raise awareness through their own events.
+                            </p>
+                            <p>
+                                For more information on Siddhartha’s Intent, please visit: <a href="https://www.siddharthasintent.org/about-us-2/our-organization/">https://www.siddharthasintent.org</a>
+                            </p>    
+                        </div>
+                        <div class="col-sm-3">
+                            <div class="thumbnail">
+                                <img src="/imgs/partner-logos/SI_logo-white.jpg" class="img-responsive">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-sm-12">
+                            <h3>Buddhist Digital Resource Center</h3>
+                        </div>
+                        <div class="col-sm-9">
+                            <p>
+                                BDRC is a non-profit dedicated to seeking out, preserving, organizing, and disseminating Buddhist literature. Joining digital technology with scholarship, BDRC ensures that the ancient wisdom and cultural treasures of the Buddhist literary tradition are not lost, but are made available for future generations. It was founded as the Tibetan Buddhist Resource Center in 1999 by E. Gene Smith (1936-2010), and since then, BDRC has located, digitized, cataloged, and archived over 15 million pages of culturally significant Buddhist works. BDRC stores the works in its secure archive, built to ensure that the texts remain free and accessible to the public into the indefinite future. Today, BDRC's library is a crucial resource for Tibetan studies, relied on by an international community of academic scholars, religious leaders, translators, Tibetan scholars, publishers, and the interested public.
+In 2016, the Tibetan Buddhist Resource Center expanded its mission and became the Buddhist Digital Resource Center. In addition to continuing the work started by E. Gene Smith, BDRC is now tasked with preserving and disseminating all Buddhist literary traditions that remain endangered due to precarious social and environmental factors.
+                            </p>
+                            <p>
+                                Through complimentary visions, BDRC helps 84000 locate source texts???
+                            </p>
+                            <p>
+                                For more information on BDRC, please visit: <a href="www.tbrc.org ">www.tbrc.org </a>
+                            </p>
+                        </div>
+                        <div class="col-sm-3">
+                            <div class="thumbnail">
+                                <img src="/imgs/partner-logos/tbrc.png" class="img-responsive">
+                            </div>
+                        </div>
+                    </div>
+                    <!--
+                    <div class="row">
+                        <div class="col-sm-12">
+                            <h3>Yuan Man Fa Zang</h3>
+                        </div>
+                        <div class="col-sm-9">
+                            <p>
+                                xxxxxxxxxxxxxxxx xxxxxxxxxxxxxxxx xxxxxxxxxxxxxxxx xxxxxxxxxxxxxxxx xxxxxxxxxxxxxxxx
+                            </p>
+                            <p>
+                                For more information on YMFZ, please visit: <a href=""></a>
+                            </p>
+                        </div>
+                        <div class="col-sm-3">
+                            <div class="thumbnail">
+                                <img src="/imgs/partner-logos/" class="img-responsive">
+                            </div>
+                        </div>
+                    </div>
+                    -->
+                </div>
+
+                <div class="panel-footer sharing">
+                    Share this page: 
+                    <a href="#" target="_blank"><i class="fa fa-facebook-square" aria-hidden="true"></i></a>
+                    <a href="#" target="_blank"><i class="fa fa-twitter-square" aria-hidden="true"></i></a>
+                    <a href="#" target="_blank"><i class="fa fa-google-plus-square" aria-hidden="true"></i></a>
+                </div>
+                
+            </div>
+        </div>
+        <div class="col-md-3 col-md-merge-left col-md-pad-top">
+            <?php 
+
+                include("sponsor-stats.php");
+
+            ?>
+        </div>
+
+    </div>
+
+</div>

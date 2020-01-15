0e1b7d65896208d3fa79c26d3d398086f409705e
domlatham <dominiclatham@gmail.com>
2020-01-15 17:32:23 +0100

    sponsor forms

--- test-pages/donate-success.php
+++ test-pages/donate-success.php
@@ -4,11 +4,21 @@
         <div class="col-md-9 col-md-merge-right">
             <div class="panel panel-default main-panel panel-how-you-can-help foreground">
 
-                <div class="panel-body text-center min-height-md">
+                <div class="panel-img-header thumbnail has-img">
+                    <img src="/imgs/DSCN0799-2.jpg" alt="header image" class="stretch">
+                    <h1>Download Dāna</h1>
+                </div>
+
+                <div class="panel-body">
+                    <blockquote>
+                        <p>It is entirely possible that the survival of the Buddhadharma could depend on it being translated into other languages.</p>
+                        <footer>Dzongsar Khyentse Rinpoche</footer>
+                    </blockquote>
+
                     <h1>Your donation was successful!</h1>
                     <p>Thank you for you generous support. We hope you enjoy the translation.</p>
                 </div>
-                
+
             </div>
         </div>
         <div class="col-md-3 col-md-merge-left col-md-pad-top">
--- test-pages/donate.php
+++ test-pages/donate.php
@@ -10,7 +10,7 @@
                     </div>
                 <?php } else { ?>
                     <?php if($_GET['lang'] !== 'zh') { ?>
-                        <div class="alert alert-info alert-dismissible fade in text-center" role="alert">
+                        <div class="alert alert-success alert-dismissible fade in text-center" role="alert">
                             <button type="button" class="close" data-dismiss="alert" aria-label="Close">
                                 <span aria-hidden="true">&times;</span>
                             </button>
@@ -57,7 +57,7 @@
 
                             <div class="col-lg-7">
 
-                                <h4 class="no-top-margin">I would like to offer the following dāna for my download:</h4>
+                                <!-- <h4 class="no-top-margin">I would like to offer the following dāna for my download:</h4> -->
 
                                 <div class="form-group">
                                     <label for="amount1" class="col-sm-4 control-label">Select amount</label>
@@ -85,37 +85,9 @@
                                     </div>
                                 </div>
 
-                                <hr>
-
-                                <div class="form-group">
-                                    <div class="col-md-offset-4 col-md-8 col-lg-offset-0 col-lg-12">
-                                        <div class="checkbox">
-                                            <label>
-                                                <input autocomplete="off" name="FieldName24004" type="hidden" value="MarkAsAnonymous">
-                                                <input type="checkbox" name="FieldValue24004" value="yes"> Make this an anonymous donation
-                                            </label>
-                                        </div>
-                                        <p class="text-muted small">We nevertheless require your personal details to process payment.</p>
-                                    </div>
-                                </div>
-
                                 <?php if($_GET['lang'] !== 'zh') { ?>
 
                                     <!-- English name -->
-                                    <div class="form-group">
-                                        <input autocomplete="off" name="FieldName2600" type="hidden" value="Title">
-                                        <label for="title" class="col-sm-4 control-label">Title</label>
-                                        <div class="col-sm-4">
-                                            <select name="FieldValue2600" id="title" class="form-control">
-                                                <option value=""></option>
-                                                <option value="mr">Mr.</option>
-                                                <option value="ms">Ms.</option>
-                                                <option value="mrs">Mrs.</option>
-                                                <option value="dr">Dr.</option>
-                                                <option value="prof">Prof.</option>
-                                            </select>
-                                        </div>
-                                    </div>
 
                                     <div class="form-group">
                                         <label for="fname" class="col-sm-4 control-label">First name</label>
@@ -136,24 +108,13 @@
                                     <!-- Chinese name -->
                                     <div class="form-group">
                                         <label for="chineseName" class="col-sm-4 control-label">Name</label>
-                                        <div class="col-sm-7">
+                                        <div class="col-sm-8">
                                             <input type="hidden" name="FieldName26009" value="ChineseName">
                                             <input type="text" name="FieldValue26009" class="form-control" id="chineseName" placeholder="" required="required">
                                         </div>
                                     </div>
 
                                 <?php } ?>
-
-                                <div class="form-group">
-                                    <input type="hidden" id="BillingPhone" name="BillingPhone" size="30">
-                                    <label for="phone" class="col-xs-12 col-sm-4 control-label">Tel. (optional)</label>
-                                    <div class="col-sm-3">
-                                        <?php include('select-country-phone.php') ?>
-                                    </div>
-                                    <div class="col-sm-5">
-                                        <input type="tel" name="callingmainphone" class="form-control" id="phoneLocal" placeholder="" pattern="\d{5,14}" data-merge-values-on-change="#BillingPhone">
-                                    </div>
-                                </div>
 
                                 <div class="form-group">
                                     <label for="email" class="col-sm-4 control-label">Email address</label>
@@ -184,170 +145,7 @@
                                     </div>
                                 </div>
 
-                                <div class="tabs-container top-margin">
-
-                                    <input type="hidden" name="PaymentType" id="paymentType" value="CreditCard">
-
-                                    <ul class="nav nav-tabs" role="tablist">
-                                        <li role="presentation" class="active">
-                                            <a href="#payment-cc" aria-controls="payment-cc" role="tab" data-toggle="tab" data-onclick-set='{"#paymentType": "CreditCard"}' data-input-required="#cardName,#cardNumber,#cvvNumber,#expiryYear">
-                                                Credit Card
-                                            </a>
-                                        </li>
-                                        <li role="presentation">
-                                            <a href="#payment-ft" aria-controls="payment-ft" role="tab" data-toggle="tab" data-onclick-set='{"#paymentType": "Check"}' data-input-required="#accountName,#routingNumber,#accountNumber">
-                                                Electronic Funds Transfer
-                                            </a>
-                                        </li>
-                                    </ul>
-
-                                    <div class="tab-content">
-                                        <div role="tabpanel" class="tab-pane active" id="payment-cc">
-
-                                            <div class="form-group">
-                                                <label for="cardName" class="col-sm-4 control-label">Name on card</label>
-                                                <div class="col-sm-8">
-                                                    <input type="text" name="NameOnCard" class="form-control" id="cardName" placeholder="" required="required">
-                                                </div>
-                                            </div>
-
-                                            <div class="form-group">
-                                                <label for="cardNumber" class="col-sm-4 control-label">Card Number</label>
-                                                <div class="col-sm-8">
-                                                    <input type="text" name="CardNumber" class="form-control" id="cardNumber" placeholder="" required="required" pattern="\d{16,19}">
-                                                </div>
-                                            </div>
-
-                                            <div class="form-group vertical-align">
-                                                <label for="cvvNumber" class="col-sm-4 control-label">CVV Number</label>
-                                                <div class="col-sm-8">
-                                                    <div class="center-vertical">
-                                                        <span>
-                                                            <input type="number" name="Cvv2" class="form-control" id="cvvNumber" placeholder="" required="required" pattern="\d{3,4}" style="width:100px;">
-                                                        </span>
-                                                        <span>
-                                                            <a href="#iframe-modal" class="small" data-toggle="modal" data-target="#iframe-modal" data-href="https://www.cvvnumber.com/cvv.html">
-                                                                What is a CVV number?
-                                                            </a>
-                                                        </span>
-                                                    </div>
-                                                </div>
-                                            </div>
-
-                                            <div class="form-group">
-                                                <label for="expiryMonth" class="col-xs-12 col-sm-4 control-label">Expiry</label>
-                                                <div class="col-xs-6 col-sm-4">
-                                                    <select id="expiryMonth" name="ExpirationMonth" class="form-control">
-                                                        <option value="01">January</option>
-                                                        <option value="02">February</option>
-                                                        <option value="03">March</option>
-                                                        <option value="04">April</option>
-                                                        <option value="05">May</option>
-                                                        <option value="06">June</option>
-                                                        <option value="07">July</option>
-                                                        <option value="08">August</option>
-                                                        <option value="09">September</option>
-                                                        <option value="10">October</option>
-                                                        <option value="11">November</option>
-                                                        <option value="12">December</option>
-                                                    </select>
-                                                </div>
-                                                <div class="col-xs-6 col-sm-4">
-                                                    <input type="number" name="ExpirationYear" id="expiryYear" class="form-control" placeholder="YYYY" pattern="\d{4}">
-                                                </div>
-                                            </div>
-
-                                        </div>
-
-                                        <div role="tabpanel" class="tab-pane" id="payment-ft">
-                                            
-                                            <div class="form-group">
-                                                <label for="accountType" class="col-sm-4 control-label">Account Type</label>
-                                                <div class="col-sm-4">
-                                                    <div class="radio">
-                                                        <label>
-                                                            <input type="radio" name="AccountType" value="CheckingAccount" id="accountType" checked="checked"> Checking
-                                                        </label>
-                                                    </div>
-                                                </div>
-                                                <div class="col-sm-4">
-                                                    <div class="radio">
-                                                        <label>
-                                                            <input type="radio" name="accountType" value="SavingsAccount"> Savings
-                                                        </label>
-                                                    </div>
-                                                </div>
-                                            </div>
-
-                                            <div class="form-group">
-                                                <label for="accountName" class="col-sm-4 control-label">Name on account</label>
-                                                <div class="col-sm-8">
-                                                    <input type="text" name="NameOnAccount" class="form-control" id="accountName" placeholder="">
-                                                </div>
-                                            </div>
-
-                                            <div class="form-group">
-                                                <label for="routingNumber" class="col-sm-4 control-label">Routing number</label>
-                                                <div class="col-sm-8">
-                                                    <input type="text" name="RoutingNumber" class="form-control" id="routingNumber" placeholder="">
-                                                </div>
-                                            </div>
-
-                                            <div class="form-group">
-                                                <label for="accountNumber" class="col-sm-4 control-label">Account number</label>
-                                                <div class="col-sm-8">
-                                                    <input type="text" name="AccountNumber" class="form-control" id="accountNumber" placeholder="">
-                                                </div>
-                                            </div>
-                                        </div>
-
-                                        <div class="form-group">
-                                            <label for="address1" class="col-sm-4 control-label">Billing address</label>
-                                            <div class="col-sm-8">
-                                                <input type="text" name="BillingAddress1" class="form-control" id="address1" placeholder="" required="required">
-                                            </div>
-                                        </div>
-
-                                        <div class="form-group">
-                                            <label for="address2" class="col-sm-4 control-label sr-only">Address 2</label>
-                                            <div class="col-sm-offset-4 col-sm-8">
-                                                <input type="text" name="BillingAddress2" class="form-control" id="address2" placeholder="">
-                                            </div>
-                                        </div>
-
-                                        <div class="form-group">
-                                            <label for="addressCity" class="col-sm-4 control-label">City</label>
-                                            <div class="col-sm-5">
-                                                <input type="text" name="BillingCity" class="form-control" id="addressCity" placeholder="">
-                                            </div>
-                                        </div>
-
-                                        <div class="form-group">
-                                            <label for="addressCountry" class="col-sm-4 control-label">Country</label>
-                                            <div class="col-sm-8">
-
-                                                <?php include('select-country-address.php') ?>
-
-                                            </div>
-                                        </div>
-
-                                        <div class="form-group">
-                                            <label for="addressState" class="col-sm-4 control-label">State / province</label>
-                                            <div class="col-sm-5">
-                                                <input type="text" name="BillingStateProvince" class="form-control" id="addressState" placeholder="">
-                                            </div>
-                                        </div>
-
-                                        <div class="form-group">
-                                            <label for="addressZip" class="col-sm-4 control-label">Postcode / Zip</label>
-                                            <div class="col-sm-5">
-                                                <input type="text" name="BillingPostalCode" class="form-control" id="addressZip" placeholder="" required="required">
-                                            </div>
-                                        </div>
-
-                                    </div>
-
-                                </div>
+                                <?php include('payment-options.php') ?>
 
                                 <hr>
 
@@ -356,15 +154,16 @@
                                         <input type="submit" class="btn btn-primary" value="Submit Your Donation">
                                     </div>
                                 </div>
+
+                                <div class="alert alert-warning text-center small">
+                                    <p>All donations are considered unrestricted contributions, enabling 84000 to carry out their goals of translation and global access efficiently and effectively</p>    
+                                </div>
+
                             </div>
 
                             <hr class="hidden-lg">
 
                             <div class="col-lg-5 border-left-lg">
-
-                                <div class="alert alert-warning text-center small">
-                                    <p>All donations are considered unrestricted contributions, enabling 84000 to carry out their goals of translation and global access efficiently and effectively</p>    
-                                </div>
 
                                 <h3 class="no-top-margin">What is Dāna?</h3>
                                 <p><em>Dāna</em> is the practice of voluntarily giving, the cultivation of generosity, the donation of alms, material, energy, or wisdom. In its broadest form, it is considered one of the most fundamental of all Buddhist practices.</p>
